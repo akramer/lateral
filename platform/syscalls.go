6d3eb5ad73160b0d47a3b681aca4fcffec9a8b39
Adam Kramer <adam@the-kramers.net>
2016-05-16 06:09:59 +0000

    cleaning up comments for syscall

--- platform/syscalls.go
+++ platform/syscalls.go
@@ -1,9 +1,8 @@
-// Implement the getsid system call.
-// This appears to exist on Linux and FreeBSD at the very least.
 package platform
 
 import "syscall"
 
+// Getsid implements the missing half of Setsid in syscall
 func Getsid(pid int) (sid int, err error) {
 	r0, _, e1 := syscall.Syscall(syscall.SYS_GETSID, uintptr(pid), 0, 0)
 	sid = int(r0)
@@ -13,6 +12,7 @@ func Getsid(pid int) (sid int, err error) {
 	return
 }
 
+// Getfd returns true if fd is close on exec
 func Getfd(fd int) (cloexec bool, err error) {
 	r0, _, e1 := syscall.Syscall(syscall.SYS_FCNTL, uintptr(fd), 0, 0)
 	cloexec = int(r0)&syscall.FD_CLOEXEC != 0
@@ -22,6 +22,7 @@ func Getfd(fd int) (cloexec bool, err error) {
 	return
 }
 
+// Stat performs a stat without creating an os.File that has close-on-garbage-collect semantics
 func stat(fd int) (*syscall.Stat_t, error) {
 	var stat syscall.Stat_t
 	err := syscall.Fstat(fd, &stat)
